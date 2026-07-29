6b32cbde067b86981129c8d4a3a25ba7861dd514
Amal Benfateh <amal.benfateh@gmail.com>
2026-07-29 17:16:10 +0100

    advanced operations matrix
    Added example usage of matrix operations including addition, multiplication, transpose, determinant, and inverse.

---
+++ Math Foundations/basics/advanced/matixAdv.py
@@ -0,0 +1,92 @@
+class Matrix:
+    def __init__(self, data):
+        self.data = [list(row) for row in data]
+        self.rows = len(self.data)
+        self.cols = len(self.data[0])
+        self.shape = (self.rows, self.cols)
+
+    def __repr__(self):
+        rows_str = "\n  ".join(str(row) for row in self.data)
+        return f"Matrix({self.shape}):\n  {rows_str}"
+
+    def __add__(self, other):
+        return Matrix([
+            [self.data[i][j] + other.data[i][j] for j in range(self.cols)]
+            for i in range(self.rows)
+        ])
+
+    def __sub__(self, other):
+        return Matrix([
+            [self.data[i][j] - other.data[i][j] for j in range(self.cols)]
+            for i in range(self.rows)
+        ])
+
+    def scalar_multiply(self, scalar):
+        return Matrix([
+            [self.data[i][j] * scalar for j in range(self.cols)]
+            for i in range(self.rows)
+        ])
+
+    def element_wise_multiply(self, other):
+        return Matrix([
+            [self.data[i][j] * other.data[i][j] for j in range(self.cols)]
+            for i in range(self.rows)
+        ])
+
+    def matmul(self, other):
+        return Matrix([
+            [
+                sum(self.data[i][k] * other.data[k][j] for k in range(self.cols))
+                for j in range(other.cols)
+            ]
+            for i in range(self.rows)
+        ])
+
+    def transpose(self):
+        return Matrix([
+            [self.data[j][i] for j in range(self.rows)]
+            for i in range(self.cols)
+        ])
+
+    def determinant(self):
+        if self.shape == (1, 1):
+            return self.data[0][0]
+        if self.shape == (2, 2):
+            return self.data[0][0] * self.data[1][1] - self.data[0][1] * self.data[1][0]
+        det = 0
+        for j in range(self.cols):
+            minor = Matrix([
+                [self.data[i][k] for k in range(self.cols) if k != j]
+                for i in range(1, self.rows)
+            ])
+            det += ((-1) ** j) * self.data[0][j] * minor.determinant()
+        return det
+
+    def inverse_2x2(self):
+        det = self.determinant()
+        if det == 0:
+            raise ValueError("Matrix is singular, no inverse exists")
+        return Matrix([
+            [self.data[1][1] / det, -self.data[0][1] / det],
+            [-self.data[1][0] / det, self.data[0][0] / det]
+        ])
+
+    @staticmethod
+    def identity(n):
+        return Matrix([
+            [1 if i == j else 0 for j in range(n)]
+            for i in range(n)
+        ])
+#------examples----
+
+A = Matrix([[1, 2], [3, 4]])
+B = Matrix([[5, 6], [7, 8]])
+
+print("A + B =", (A + B).data)
+print("A @ B =", A.matmul(B).data)
+print("A^T =", A.transpose().data)
+print("det(A) =", A.determinant())
+print("A^-1 =", A.inverse_2x2().data)
+
+I = Matrix.identity(2)
+print("A @ A^-1 =", A.matmul(A.inverse_2x2()).data)
